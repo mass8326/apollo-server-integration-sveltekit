1fa1308c1ddeeed0570ac82745352bddd9870b5c
Mass8326 <reyf.mag@gmail.com>
2023-03-01 20:00:45 -0500

    REFACTOR: Move handler creation into server route

--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,7 +1,16 @@
-export {
-	handler as GET,
-	handler as POST,
-	handler as PATCH,
-	handler as PUT,
-	handler as DELETE,
-} from "$lib/server/handler";
+import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+import { startServerAndCreateSvelteHandler } from "$lib/apollo";
+import { context } from "$lib/server/context";
+import { resolvers, typeDefs } from "$lib/server/schema";
+
+const server = new ApolloServer({
+	cache: "bounded",
+	typeDefs,
+	resolvers,
+	introspection: true,
+	plugins: [ApolloServerPluginLandingPageLocalDefault()],
+});
+const handler = startServerAndCreateSvelteHandler(server, { context });
+
+export { handler as GET, handler as POST };
